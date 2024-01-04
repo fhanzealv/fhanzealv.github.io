320e7fb5579c2cb1372f7e6e655325ea2c71fb0c
Fhanze <fhanze.id@gmail.com>
2024-01-04 20:57:28 +0700

    change script

--- script.js
+++
@@ -1,7 +0,0 @@
-const hamburger = document.querySelector('#hamburger');
-const navlink = document.querySelector('#navlink');
-
-hamburger.addEventListener('click', function(){
-    navlink.classList.toggle('block');
-
-});

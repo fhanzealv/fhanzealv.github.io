ea06ef93ff2a7a27ba866f33c0afd1d47e31cec8
Fhanze <fhanze.id@gmail.com>
2024-02-04 15:24:00 +0700

    change index

--- dist/script.js
+++ dist/script.js
@@ -1,8 +1,12 @@
 //hamburger
 const hamburger = document.querySelector('#hamburger');
 const navMenu = document.querySelector('#nav__links');
+const navRight = document.querySelector('#nav__right');
+const brand = document.querySelector('#brand');
 
 hamburger.addEventListener('click', function () {
 hamburger.classList.toggle('hamburger-active');
+brand.classList.toggle('brand-active');
 navMenu.classList.toggle('hidden');
+navRight.classList.toggle('hidden');
 });
